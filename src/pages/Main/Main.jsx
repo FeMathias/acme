607c72f408e34b6862e4c16fb862fd3538b424b3
Felipe Mathias <fmathias.dev@gmail.com>
2022-06-08 09:40:56 -0300

    final-commit

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -7,10 +7,13 @@ const Main = () => {
     <div className='main__container'> <Head title={`ACME | Home`} />
       <section className='main__top-grid'>
         <div className='main__top-grid-left'>
+          <p>COMPRE SUA BIGORNA COM 80% DE DESCONTO!</p>
         </div>
         <div className='main__top-grid-right-upper'>
+          <p>COMPRE SUA BIGORNA COM 80% DE DESCONTO!</p>
         </div>
         <div className='main__top-grid-right-lower'>
+         <p>COMPRE SUA BIGORNA COM 80% DE DESCONTO!</p>
         </div>
       </section>
     </div>
